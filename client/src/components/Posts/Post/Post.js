1314cc24f67de69e94f75d0ad18eed16716e42fb
Ankan Pal <ankan1811@gmail.com>
2021-02-20 16:35:29 +0530

    Update Post.js

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -38,7 +38,7 @@ const Post = ({ post, setCurrentId }) => { //post represent the props and accept
       </CardContent>
 
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button> //Like post button
+        <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" />  Like {post.likeCount} </Button> //Like post button
         <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button> //Delete post button
       </CardActions>
     </Card>
