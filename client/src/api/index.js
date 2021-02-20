6be829ff96448f1abf58545cd8ad747cc2ef5a5e
Ankan Pal <ankan1811@gmail.com>
2021-02-20 17:35:31 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -7,7 +7,7 @@ export const createPost = (newPost) => axios.post(url, newPost);
 //A POST request can be made using Axios to “post” data to an endpoint. 
 //This endpoint may then use this POST request to perform a certain task or trigger an event.
 // The HTTP post request is performed by calling axios.post().
-//After we get the post we have to specify the url and data we are sending
+//After we get the created post we have to specify the url and data we are sending
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
 
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost); //We receive the id and the updated post and do a patch request to the backend 
