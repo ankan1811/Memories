d7562020c18c886bcb4520aead40421a492a299e
Ankan Pal <ankan1811@gmail.com>
2021-02-20 11:59:50 +0530

    Update Post.js

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -9,16 +9,17 @@ import { useDispatch } from 'react-redux';
 import { likePost, deletePost } from '../../../actions/posts';
 import useStyles from './styles';
 
-const Post = ({ post, setCurrentId }) => {
+const Post = ({ post, setCurrentId }) => { //post represent the props
   const dispatch = useDispatch();
   const classes = useStyles();
 
-  return (//fromNow means 5 secs from now or 5 mins from now like that
-    <Card className={classes.card}>
+  return (
+    <Card className={classes.card}> //selectedFile is poresent in schema in backend
       <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
       <div className={classes.overlay}>
         <Typography variant="h6">{post.creator}</Typography>
         <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+//moment has something called fromNow which means 5 secs from now or 5 mins from now like that
       </div>
       <div className={classes.overlay2}>
         <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}>
@@ -27,6 +28,7 @@ const Post = ({ post, setCurrentId }) => {
       </div>
       <div className={classes.details}>
         <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
+    //we will look over our tags and for each tag just add a hashtag
       </div>
 
       <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
