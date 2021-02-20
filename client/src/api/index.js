aa8f18ef4ea5a95f5471afa379e26d00d2a8869e
Ankan Pal <ankan1811@gmail.com>
2021-02-20 13:34:06 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -9,5 +9,7 @@ export const createPost = (newPost) => axios.post(url, newPost);
 // The HTTP post request is performed by calling axios.post().
 //After we get the post we have to specify the url and data we are sending
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost); //We receive the id and the updated post and do a patch request to the backend 
+// and the backend is connected to the database
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
