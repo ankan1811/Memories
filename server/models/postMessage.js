f506d939a204adda61fe2419e80a1d080fd53697
Ankan Pal <ankan1811@gmail.com>
2021-03-31 18:43:43 +0530

    Update postMessage.js

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 //Mongoose schema
+//A Mongoose schema is a json object which defines the structure of the document, default values, validators, etc.,
+// whereas a Mongoose model provides an interface to the database for creating, querying, updating, deleting records, etc.
 const postSchema = mongoose.Schema({
     title: String,
     message: String,
