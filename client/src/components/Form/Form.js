1b82ce68672f23a72ebc5be4b44102707fab4f7f
Ankan Pal <ankan1811@gmail.com>
2021-04-01 22:04:27 +0530

    Update Form.js

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -11,7 +11,7 @@ const Form = ({ currentId, setCurrentId }) => { //we are accepting them as props
   //This hook is only used in this file only because here we are only concerned with the data present in the post We are not performing any action
   //At the beginning everything will be an empty string.
   const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
-  //We only want to fetch the new updated post.
+  //We only want to fetch the new updated post so that we get all the information about it.
   //if we have a currentid(i.e. not null) we will loop over state.posts and we want to find a post that has the same id as our current id else
   //if we do not have current id return null
 
