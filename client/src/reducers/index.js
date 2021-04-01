453b5d02e90593461f122b19a490199cb5e98cea
Ankan Pal <ankan1811@gmail.com>
2021-04-01 12:10:52 +0530

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,6 +3,9 @@ import { combineReducers } from 'redux';
 import posts from './posts'; //reducers will be used in posts
 
 export const reducers = combineReducers(
+    //The combineReducers helper function turns an object whose values are different reducing functions into a single reducing function you can pass to createStore . 
+  //The resulting reducer calls every child reducer, and gathers their results into a single state object
+  
   //We will use all individual reducers that we have
   { posts } //posts:posts 
 );
